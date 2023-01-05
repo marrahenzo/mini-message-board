bf199bf652164c229b71c35e9db471d6ebae92aa
Hernán Marrapodi <69157896+marrahenzo@users.noreply.github.com>
2023-01-05 12:29:38 -0300

    Add get and post handlers for form

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,17 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+  messages.push({
+    text: req.body.name,
+    user: req.body.message,
+    added: new Date()
+  });
+  res.redirect('/');
+});
+
 module.exports = router;
